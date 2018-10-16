236134dc7156ac92bf18026c537e5bc10202972e
Damian Juzyk <damian.juzyk@gmail.com>
2018-10-16 17:12:28 +0200

    add iterator 2

---
+++ src/iteracje/Names.java
@@ -0,0 +1,28 @@
+package iteracje;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Names {
+    public static void main(String[] args) {
+        Set <String> names = new TreeSet<>();
+        names.add("Ola");
+        names.add("Kasia");
+        names.add("Krzysiek");
+        names.add("Roman");
+        names.add("Aneta");
+
+        Iterator <String> iterator = names.iterator();
+        while(iterator.hasNext()) {
+            String next = iterator.next();
+            System.out.println(next);
+        }
+
+        for (String num : names) {
+            System.out.println(names);
+        }
+
+
+    }
+}
